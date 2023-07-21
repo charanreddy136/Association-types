943565cb1a1a7b2e40df3874ea6722b08ee75bc7
charanreddy136 <charanreddy136@gmail.com>
2023-07-21 23:17:43 +0530

    First commit

---
+++ one-Many Association( Constructor Injecting)/src/com/gqt/entities1/Employee.java
@@ -0,0 +1,24 @@
+package com.gqt.entities1;
+
+public class Employee 
+{
+	private int empId;
+	private String empName;
+	
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	
+
+}
---
+++ one-Many Association( Constructor Injecting)/src/com/gqt/test2/Launch2.java
@@ -0,0 +1,28 @@
+package com.gqt.test2;
+
+import com.gqt.entities1.Department;
+import com.gqt.entities1.Employee;
+
+public class Launch2
+{
+		public static void main(String[] args)
+		{
+			//creating dependent object
+			Employee emp1 = new Employee(18,"sachin");
+			Employee emp2 = new Employee(24,"dhoni");
+			Employee emp3 = new Employee(42,"zabi");
+			
+			//Creating an array to perform one-many association
+			Employee e[] =new Employee[3];
+			
+			e[0] = emp1;
+			e[1] = emp2;
+			e[2] = emp3;
+			
+			//creating Target object using Constructor
+			Department d = new Department(123,"Bcci","Dubai",e);
+			
+			d.disp();
+
+		}
+}
